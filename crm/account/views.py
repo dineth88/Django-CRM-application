56e9bbb8f42699283912614a0a0de052e1f8c7a6
dineth88 <dinethoshitha00@gmail.com>
2025-04-17 22:36:32 +0530

    Added initial commit

---
+++ crm/account/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'account/dashboard.html')
+
+def product(request):
+    return render(request, 'account/products.html')
+
+def customer(request):
+    return render(request, 'account/customer.html')
